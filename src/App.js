f49076a29338a9f00ac612d494b14ac3a5b00e47
luckmer <woszczo04@gmail.com>
2020-08-08 18:58:09 +0200

    src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { routes } from "./routes";
+import DataContext from "./data/Data";
+import Nav from "./components/nav";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const location = useLocation();
+    return (
+        <>
+            <Nav />
+            <DataContext>
+                <Switch location={location} key={location.pathname}>
+                    {routes.map(({ path, Component }) => (
+                        <Route
+                            key={path}
+                            exact
+                            path={path}
+                            component={Component}
+                        />
+                    ))}
+                </Switch>
+            </DataContext>
+        </>
+    );
 }
 
 export default App;
