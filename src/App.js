a9fb7d3b73a1e89a4022a8663d57e9559f4d604e
luckmer <woszczo04@gmail.com>
2020-08-09 10:20:03 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { routes } from "./routes";
 import DataContext from "./data/Data";
 import Nav from "./components/nav";
+import { ThemeProvider } from "styled-components";
+import Box from "./styles/Box";
 function App() {
     const location = useLocation();
+    const [type, setType] = useState({
+        mode: "light",
+    });
+    const Color = type.mode;
+
+    const toggleChange = () => {
+        setType({ mode: Color === "light" ? "dark" : "light" });
+    };
     return (
-        <div>
-            <Nav />
-            <DataContext>
-                <Switch location={location} key={location.pathname}>
-                    {routes.map(({ path, Component }) => (
-                        <Route
-                            key={path}
-                            exact
-                            path={path}
-                            component={Component}
-                        />
-                    ))}
-                </Switch>
-            </DataContext>
-        </div>
+        <ThemeProvider theme={{ mode: Color }}>
+            <Box>
+                <Nav type={Color} toggleChange={toggleChange} />
+                <DataContext>
+                    <Switch location={location} key={location.pathname}>
+                        {routes.map(({ path, Component }) => (
+                            <Route
+                                key={path}
+                                exact
+                                path={path}
+                                component={Component}
+                            />
+                        ))}
+                    </Switch>
+                </DataContext>
+            </Box>
+        </ThemeProvider>
     );
 }
 
